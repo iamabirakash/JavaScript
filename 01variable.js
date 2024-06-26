dd2527fa9494f393fc801724073b19bbf1ca31b0
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-06-26 20:35:48 +0530

    Added some comments end of 04 lecture

--- 01variable.js
+++ 01variable.js
@@ -1,7 +1,10 @@
 const accountId = "abcd"
 let accountEmail = "abcd@google.com"
 var accountPassword = "abcd1234"
-accountCity = "Jaipur"
+accountCity = "Jaipur"  //it is legit in JS but we avoid practicing it
+let accountState;  //the output will show UNDEFINED
 
 console.log(accountEmail);
-console.table([accountId,accountEmail,accountPassword])
+console.table([accountId,accountEmail,accountPassword, accountState])
+
+//prefer not to use var because of issue in block and functional scope
