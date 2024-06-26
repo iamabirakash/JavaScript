1ea0740d462b758ba9549715f973aa1fb84f9984
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-06-26 21:07:56 +0530

    Conversion | End of 6th video

---
+++ 03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = 53
+// console.log(typeof(score));
+
+// let score = "53abc"
+// console.log(typeof(score));
+// let convertNumber = Number(score)
+// console.log(convertNumber);
+// console.log(typeof(convertNumber));
+
+// let score = null
+// console.log(typeof(score));
+// let convertNumber = Number(score)
+// console.log(convertNumber);
+// console.log(typeof(convertNumber));
+
+// let score = true
+// console.log(typeof(score))
+// let convertNumber = Number(score)
+// console.log(convertNumber)
+// console.log(typeof(convertNumber))
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1
