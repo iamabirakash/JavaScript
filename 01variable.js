c056db8e0b1c088b33607a177b10d62947952204
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-06-26 20:27:38 +0530

    added js

---
+++ 01variable.js
@@ -0,0 +1,7 @@
+const accountId = "abcd"
+let accountEmail = "abcd@google.com"
+var accountPassword = "abcd1234"
+accountCity = "Jaipur"
+
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword])
