8e67b700a9930b26d20f828b5c042a6cb0f443f8
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-06-26 20:57:15 +0530

    DATA TYPES | End of 5th video

---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" //treats all JS code as newer version
+
+// alert(3 +3) as we are using node.js not browser
+
+//ecmaScript is a standard
+
+//for documentation tc39 and mdm
+
+let name = "abir"
+let age = 18
+let isLoggedIn = false
+
+// number => 2^53
+// big int
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => no value defined
+// symbol => to find uniqueness
+// object
+
+console.log(typeof("hello"));
