319855e51d082837f76eb1d6d71717a7e8971b12
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-06-26 22:32:41 +0530

    Date and Time | End of 13th video

---
+++ 04_date.js
@@ -0,0 +1,23 @@
+let myDate = new Date()
+
+// console.log(typeof(myDate));
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+
+// let myCreatedDate = new Date(2023,1,23,5,3)
+// let myCreatedDate = new Date("01-14-2023")
+
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
