21564b3af6aab840278f36ced64171d9f0c6812a
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-06-27 21:01:39 +0530

    Objects | End of 17th Video

---
+++ 05_objects.js
@@ -0,0 +1,45 @@
+// const helloUser = new Object();   SINGLETON
+// const helloUser = {};   NOT A SINGLETONE
+// helloUser.id = "123"
+// helloUser.name = "Hello"
+// helloUser.isLoggedIn = false
+
+// console.log(helloUser);
+
+// -----------------------------------------------------------
+
+// const helloUser = {
+//     email: "abc@google.com",
+//     fullname: {
+//         userFullName: {
+//             firstName: "Hello",
+//             lastName: "User"
+//         }
+//     }
+// }
+
+// console.log(helloUser.fullname);
+
+// ******************* MERGING **************************
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = Object.assign(obj1,obj2) //obj1 target others source,,obj1 gets updated and merged
+// const obj3 = Object.assign({},obj1,obj2) //prefered the {} is target and others are source,,,{} gets updated merged
+
+// const obj3 = {...obj1,...obj2} //prefred
+
+// console.log(obj3);
+
+
+// *************************** FOR DATABSES ***************************
+
+const helloUser = {};
+helloUser.id = "123"
+helloUser.name = "Hello"
+helloUser.isLoggedIn = false
+
+console.log(helloUser);
+console.log(Object.keys(helloUser));
+console.log(Object.values(helloUser));
